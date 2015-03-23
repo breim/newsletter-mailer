0df3e54e387cec557a263483f259ff8d5c4c2e98
Henrique Breim <henrique@breim.com.br>
2015-03-23 16:58:44 -0300

    Add logic to post on remote resource of rails

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,2 +1,19 @@
 class NewslettersController < ApplicationController
+
+	def create
+		@newsletter = Newsletter.new(email: params[:email])
+		respond_to do |format|
+			if @newsletter.save
+				format.js
+			else
+				format.js { render :file => "/newsletters/error.js.erb"  }
+			end
+		end
+	end
+
+	private
+	def company_params
+		params.require(:newsletter).permit(:email)
+	end
+
 end
